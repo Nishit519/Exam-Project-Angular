6e18f6d93ec5eb2d35cfdc10d8cd648c9b9b18ba
Nishit Chaudhary <nishit.chaudhary@oplinnovate.com>
2025-04-24 12:53:17 +0530

    Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { TableComponent } from './table/table.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'table', component: TableComponent},
-  { path: 'dashboard/:email', component: DashboardComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: 'table', component: TableComponent, canActivate: [AuthGuard]},
+  { path: 'dashboard/:email', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   {
     path: 'aZ1xR7nVf4GqL9dPbWmYcT6jHsEUkO38iJNtK5BAzvCeMyXoFDQrlpSghuZwNbRXatqVYEo7cdkpLwzTmUAgKHnFJbsiQ93vGRyLmCXeWtuqOASBxMkPnfhaVjTcLdUboqNRYmgtZKXeWJVCHQyxLiDGnUfoP2vM9zsekl8BpLtRYKhN',
-    component: ResetPasswordComponent
+    component: ResetPasswordComponent,
+    canActivate: [AuthGuard]
   }
   
 ];
